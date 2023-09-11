8f47b6b1463f896e6f5288b5453e47d667c46a3f
evie <eviezua@gmail.com>
2023-09-11 13:40:09 +0300

    Exposing API platform with entities of comment and conference

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
